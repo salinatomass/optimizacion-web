9671aa9a77b7bea9599ff4f5f9a41897de065399
Tomás Salina <salinatomass53@gmail.com>
2022-03-28 11:13:46 -0300

    Reducing the size of the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 const nodeEnv = process.env.NODE_ENV || 'development'
+const shouldAnalyze = process.argv.includes('--analyze')
+
+const plugins = []
+
+if (shouldAnalyze) plugins.push(new BundleAnalyzerPlugin())
 
 const config = {
   mode: nodeEnv,
@@ -19,6 +26,7 @@ const config = {
     historyApiFallback: true,
     port: 3005,
   },
+  plugins,
 }
 
 module.exports = config
